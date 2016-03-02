5b319ce6b22444a60939b2421d4c31686c43fe2b
Dan Finlay <dan@danfinlay.com>
2016-03-01 16:31:31 -0800

    Add second method

--- app/main.js
+++ app/main.js
@@ -9,38 +9,69 @@ import CSSTransitionGroup from 'react-addons-css-transition-group'
 import { render } from 'react-dom'
 import h from 'react-hyperscript'
 
-const reducer = (state = [], action) => {
+const reducer = (state = ['Hi Alexa'], action) => {
   switch (action.type) {
     case 'EXCITEMENT':
-      return state.concat(['!'])
+      var opts = ['?','!','@','$', '#']
+      return state.concat(opts[Math.floor(Math.random() * opts.length)])
+    case 'SADNESS':
+      return state.slice(0, state.length - 1)
+    default:
+      return state;
   }
-  return state
 }
 
 let store = createStore(reducer);
 
 class Header extends Component {
   render() {
-    const state = store.getState()
+    let message = ''
 
-    let message = 'Hi Alexa'
+    const state = store.getState()
     state.forEach(function(char) {
       message += char
     })
 
     return h('h1', {
       onClick(ev) {
-        ev.preventDefault();
+        // ev.preventDefault();
         store.dispatch({ type: 'EXCITEMENT' })
+      },
+      onKeyPress(ev) {
+        ev.preventDefault();
+        store.dispatch({ type: 'SADNESS' })
       }
     }, message)
   }
 }
 
-// Root of app:
-var container = document.getElementById('main')
-render(React.createElement(Header), container)
+class Footer extends Component {
+  render() {
 
-store.subscribe(() => render(React.createElement(Header), container));
+    return h('button', {
+      onClick(ev) {
+        // ev.preventDefault();
+        store.dispatch({ type: 'SADNESS' })
+      },
+    }, 'Make Sadder')
+  }
+}
+
+// Root of app:
+class Body extends Component {
+  render() {
+
+    return h('div', [
+      React.createElement(Header),
+      React.createElement(Footer),
+    ]);
+  }
+}
+
+
+var container = document.getElementById('main')
+render(React.createElement(Body), container)
+
+store.subscribe(() => render(React.createElement(Body), container));
 
 console.log('we win!')
