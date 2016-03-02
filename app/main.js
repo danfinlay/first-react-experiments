aabf4267833cdfc74a31179953f70ee488970c2b
Dan Finlay <dan@danfinlay.com>
2016-03-01 16:18:53 -0800

    First reactive action working!

--- app/main.js
+++ app/main.js
@@ -2,14 +2,45 @@
 
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import createFragment from 'react-addons-create-fragment'
 import immutabilityHelpers from 'react-addons-update'
 import CSSTransitionGroup from 'react-addons-css-transition-group'
 import { render } from 'react-dom'
 import h from 'react-hyperscript'
 
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case 'EXCITEMENT':
+      return state.concat(['!'])
+  }
+  return state
+}
+
+let store = createStore(reducer);
+
+class Header extends Component {
+  render() {
+    const state = store.getState()
+
+    let message = 'Hi Alexa'
+    state.forEach(function(char) {
+      message += char
+    })
+
+    return h('h1', {
+      onClick(ev) {
+        ev.preventDefault();
+        store.dispatch({ type: 'EXCITEMENT' })
+      }
+    }, message)
+  }
+}
+
 // Root of app:
 var container = document.getElementById('main')
-render(h('h1', 'Hi Alexa!!'), container)
+render(React.createElement(Header), container)
+
+store.subscribe(() => render(React.createElement(Header), container));
 
 console.log('we win!')
